18257699c5fcad0a82318c3f895ff5802ea520f1
shenyu <459240868@qq.com>
2020-09-12 00:05:43 +0800

    update

---
+++ test/test_rf.py
@@ -0,0 +1,191 @@
+from functools import partial
+import numpy as np
+from ConfigSpace.configuration_space import ConfigurationSpace
+from ConfigSpace.hyperparameters import UniformFloatHyperparameter, \
+    CategoricalHyperparameter, Constant, UnParametrizedHyperparameter, UniformIntegerHyperparameter
+from ConfigSpace.forbidden import ForbiddenEqualsClause, \
+    ForbiddenAndConjunction
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score
+from solnml.datasets.utils import load_data
+
+import pickle
+import argparse
+from litebo.facade.bo_facade import BayesianOptimization as BO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--datasets', type=str)
+parser.add_argument('--n', type=int, default=500)
+
+args = parser.parse_args()
+dataset_str = args.datasets
+run_count = args.n
+
+
+def check_datasets(datasets):
+    for _dataset in datasets:
+        try:
+            _ = load_data(_dataset, '../soln-ml/', False, task_type=0)
+        except Exception as e:
+            raise ValueError('Dataset - %s does not exist!' % _dataset)
+
+
+def check_true(p):
+    if p in ("True", "true", 1, True):
+        return True
+    return False
+
+
+def check_false(p):
+    if p in ("False", "false", 0, False):
+        return True
+    return False
+
+
+def check_none(p):
+    if p in ("None", "none", None):
+        return True
+    return False
+
+
+def check_for_bool(p):
+    if check_false(p):
+        return False
+    elif check_true(p):
+        return True
+    else:
+        raise ValueError("%s is not a bool" % str(p))
+
+
+def get_cs():
+    cs = ConfigurationSpace()
+    criterion = CategoricalHyperparameter(
+        "criterion", ["gini", "entropy"], default_value="gini")
+
+    # The maximum number of features used in the forest is calculated as m^max_features, where
+    # m is the total number of features, and max_features is the hyperparameter specified below.
+    # The default is 0.5, which yields sqrt(m) features as max_features in the estimator. This
+    # corresponds with Geurts' heuristic.
+    max_features = UniformFloatHyperparameter(
+        "max_features", 0., 1., default_value=0.5)
+
+    max_depth = UnParametrizedHyperparameter("max_depth", "None")
+    min_samples_split = UniformIntegerHyperparameter(
+        "min_samples_split", 2, 20, default_value=2)
+    min_samples_leaf = UniformIntegerHyperparameter(
+        "min_samples_leaf", 1, 20, default_value=1)
+    min_weight_fraction_leaf = UnParametrizedHyperparameter("min_weight_fraction_leaf", 0.)
+    max_leaf_nodes = UnParametrizedHyperparameter("max_leaf_nodes", "None")
+    min_impurity_decrease = UnParametrizedHyperparameter('min_impurity_decrease', 0.0)
+    bootstrap = CategoricalHyperparameter(
+        "bootstrap", ["True", "False"], default_value="True")
+    cs.add_hyperparameters([criterion, max_features,
+                            max_depth, min_samples_split, min_samples_leaf,
+                            min_weight_fraction_leaf, max_leaf_nodes,
+                            bootstrap, min_impurity_decrease])
+    return cs
+
+
+def eval_func(params, x, y):
+    params = params.get_dictionary()
+    model = RandomForest(**params)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=1)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    return 1 - balanced_accuracy_score(y_test, y_pred)
+
+
+class RandomForest:
+    def __init__(self, criterion, max_features,
+                 max_depth, min_samples_split, min_samples_leaf,
+                 min_weight_fraction_leaf, bootstrap, max_leaf_nodes,
+                 min_impurity_decrease, random_state=None, n_jobs=1,
+                 class_weight=None):
+        self.n_estimators = self.get_max_iter()
+        self.criterion = criterion
+        self.max_features = max_features
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.min_weight_fraction_leaf = min_weight_fraction_leaf
+        self.bootstrap = bootstrap
+        self.max_leaf_nodes = max_leaf_nodes
+        self.min_impurity_decrease = min_impurity_decrease
+        self.random_state = random_state
+        self.n_jobs = n_jobs
+        self.class_weight = class_weight
+        self.estimator = None
+
+    @staticmethod
+    def get_max_iter():
+        return 100
+
+    def get_current_iter(self):
+        return self.estimator.n_estimators
+
+    def fit(self, X, y, sample_weight=None):
+        from sklearn.ensemble import RandomForestClassifier
+
+        if self.estimator is None:
+            self.n_estimators = int(self.n_estimators)
+            if check_none(self.max_depth):
+                self.max_depth = None
+            else:
+                self.max_depth = int(self.max_depth)
+
+            self.min_samples_split = int(self.min_samples_split)
+            self.min_samples_leaf = int(self.min_samples_leaf)
+            self.min_weight_fraction_leaf = float(self.min_weight_fraction_leaf)
+
+            if self.max_features not in ("sqrt", "log2", "auto"):
+                max_features = int(X.shape[1] ** float(self.max_features))
+            else:
+                max_features = self.max_features
+
+            self.bootstrap = check_for_bool(self.bootstrap)
+
+            if check_none(self.max_leaf_nodes):
+                self.max_leaf_nodes = None
+            else:
+                self.max_leaf_nodes = int(self.max_leaf_nodes)
+
+            self.min_impurity_decrease = float(self.min_impurity_decrease)
+
+            # initial fit of only increment trees
+            self.estimator = RandomForestClassifier(
+                n_estimators=self.get_max_iter(),
+                criterion=self.criterion,
+                max_features=max_features,
+                max_depth=self.max_depth,
+                min_samples_split=self.min_samples_split,
+                min_samples_leaf=self.min_samples_leaf,
+                min_weight_fraction_leaf=self.min_weight_fraction_leaf,
+                bootstrap=self.bootstrap,
+                max_leaf_nodes=self.max_leaf_nodes,
+                min_impurity_decrease=self.min_impurity_decrease,
+                random_state=self.random_state,
+                n_jobs=self.n_jobs,
+                class_weight=self.class_weight,
+                warm_start=True)
+
+        self.estimator.fit(X, y, sample_weight=sample_weight)
+        return self
+
+    def predict(self, X):
+        if self.estimator is None:
+            raise NotImplementedError()
+        return self.estimator.predict(X)
+
+
+dataset_list = dataset_str.split(',')
+check_datasets(dataset_list)
+cs = get_cs()
+
+for dataset in dataset_list:
+    node = load_data(dataset, '../soln-ml/', True, task_type=0)
+    _x, _y = node.data[0], node.data[1]
+    eval = partial(eval_func, x=_x, y=_y)
+    bo = BO(eval, cs, max_runs=run_count, rng=np.random.RandomState(1))
+    bo.run()
+    with open('logs/%s-random_forest-%d.pkl' % (dataset, run_count), 'wb')as f:
+        pickle.dump(bo.get_history().data, f)
