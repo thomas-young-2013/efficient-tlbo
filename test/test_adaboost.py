43f60f340e1ac442bcc2dd8ed88d57f298d59634
shenyu <459240868@qq.com>
2020-09-15 21:28:01 +0800

    update test

---
+++ test/test_adaboost.py
@@ -0,0 +1,139 @@
+from functools import partial
+import numpy as np
+from ConfigSpace.configuration_space import ConfigurationSpace
+from ConfigSpace.hyperparameters import UniformFloatHyperparameter, \
+    CategoricalHyperparameter, Constant, UnParametrizedHyperparameter, UniformIntegerHyperparameter
+from ConfigSpace.forbidden import ForbiddenEqualsClause, \
+    ForbiddenAndConjunction
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score
+from solnml.datasets.utils import load_data
+
+import pickle
+import argparse
+from litebo.facade.bo_facade import BayesianOptimization as BO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--datasets', type=str)
+parser.add_argument('--n', type=int, default=1000)
+
+args = parser.parse_args()
+dataset_str = args.datasets
+run_count = args.n
+
+
+def check_datasets(datasets):
+    for _dataset in datasets:
+        try:
+            _ = load_data(_dataset, '../soln-ml/', False, task_type=0)
+        except Exception as e:
+            raise ValueError('Dataset - %s does not exist!' % _dataset)
+
+
+def check_true(p):
+    if p in ("True", "true", 1, True):
+        return True
+    return False
+
+
+def check_false(p):
+    if p in ("False", "false", 0, False):
+        return True
+    return False
+
+
+def check_none(p):
+    if p in ("None", "none", None):
+        return True
+    return False
+
+
+def check_for_bool(p):
+    if check_false(p):
+        return False
+    elif check_true(p):
+        return True
+    else:
+        raise ValueError("%s is not a bool" % str(p))
+
+
+def get_cs():
+    cs = ConfigurationSpace()
+
+    n_estimators = UniformIntegerHyperparameter(
+        name="n_estimators", lower=50, upper=500, default_value=50, log=False)
+    learning_rate = UniformFloatHyperparameter(
+        name="learning_rate", lower=0.01, upper=2, default_value=0.1, log=True)
+    algorithm = CategoricalHyperparameter(
+        name="algorithm", choices=["SAMME.R", "SAMME"], default_value="SAMME.R")
+    max_depth = UniformIntegerHyperparameter(
+        name="max_depth", lower=2, upper=8, default_value=3, log=False)
+
+    cs.add_hyperparameters([n_estimators, learning_rate, algorithm, max_depth])
+    return cs
+
+
+def eval_func(params, x, y):
+    params = params.get_dictionary()
+    model = AdaboostClassifier(**params)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=1)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    return 1 - balanced_accuracy_score(y_test, y_pred)
+
+
+class AdaboostClassifier:
+
+    def __init__(self, n_estimators, learning_rate, algorithm, max_depth,
+                 random_state=None):
+        self.n_estimators = n_estimators
+        self.learning_rate = learning_rate
+        self.algorithm = algorithm
+        self.random_state = random_state
+        self.max_depth = max_depth
+        self.estimator = None
+
+    def fit(self, X, Y, sample_weight=None):
+        import sklearn.tree
+
+        self.n_estimators = int(self.n_estimators)
+        self.learning_rate = float(self.learning_rate)
+        self.max_depth = int(self.max_depth)
+        base_estimator = sklearn.tree.DecisionTreeClassifier(max_depth=self.max_depth)
+
+        estimator = sklearn.ensemble.AdaBoostClassifier(
+            base_estimator=base_estimator,
+            n_estimators=self.n_estimators,
+            learning_rate=self.learning_rate,
+            algorithm=self.algorithm,
+            random_state=self.random_state
+        )
+
+        estimator.fit(X, Y, sample_weight=sample_weight)
+
+        self.estimator = estimator
+        return self
+
+    def predict(self, X):
+        if self.estimator is None:
+            raise NotImplementedError
+        return self.estimator.predict(X)
+
+    def predict_proba(self, X):
+        if self.estimator is None:
+            raise NotImplementedError()
+        return self.estimator.predict_proba(X)
+
+
+dataset_list = dataset_str.split(',')
+check_datasets(dataset_list)
+cs = get_cs()
+
+for dataset in dataset_list:
+    node = load_data(dataset, '../soln-ml/', True, task_type=0)
+    _x, _y = node.data[0], node.data[1]
+    eval = partial(eval_func, x=_x, y=_y)
+    bo = BO(eval, cs, max_runs=run_count, time_limit_per_trial=600, rng=np.random.RandomState(1))
+    bo.run()
+    with open('logs/%s-adaboost-%d.pkl' % (dataset, run_count), 'wb')as f:
+        pickle.dump(bo.get_history().data, f)
---
+++ test/test_lda.py
@@ -0,0 +1,147 @@
+from functools import partial
+import numpy as np
+from ConfigSpace.configuration_space import ConfigurationSpace
+from ConfigSpace.hyperparameters import UniformFloatHyperparameter, \
+    CategoricalHyperparameter, Constant, UnParametrizedHyperparameter, UniformIntegerHyperparameter
+from ConfigSpace.forbidden import ForbiddenEqualsClause, \
+    ForbiddenAndConjunction
+from ConfigSpace.conditions import EqualsCondition
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import balanced_accuracy_score
+from solnml.datasets.utils import load_data
+
+import pickle
+import argparse
+from litebo.facade.bo_facade import BayesianOptimization as BO
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--datasets', type=str)
+parser.add_argument('--n', type=int, default=1000)
+
+args = parser.parse_args()
+dataset_str = args.datasets
+run_count = args.n
+
+
+def check_datasets(datasets):
+    for _dataset in datasets:
+        try:
+            _ = load_data(_dataset, '../soln-ml/', False, task_type=0)
+        except Exception as e:
+            raise ValueError('Dataset - %s does not exist!' % _dataset)
+
+
+def check_true(p):
+    if p in ("True", "true", 1, True):
+        return True
+    return False
+
+
+def check_false(p):
+    if p in ("False", "false", 0, False):
+        return True
+    return False
+
+
+def check_none(p):
+    if p in ("None", "none", None):
+        return True
+    return False
+
+
+def check_for_bool(p):
+    if check_false(p):
+        return False
+    elif check_true(p):
+        return True
+    else:
+        raise ValueError("%s is not a bool" % str(p))
+
+
+def get_cs():
+    cs = ConfigurationSpace()
+    shrinkage = CategoricalHyperparameter(
+        "shrinkage", ["None", "auto", "manual"], default_value="None")
+    shrinkage_factor = UniformFloatHyperparameter(
+        "shrinkage_factor", 0., 1., 0.5)
+    n_components = UniformIntegerHyperparameter('n_components', 1, 250, default_value=10)
+    tol = UniformFloatHyperparameter("tol", 1e-5, 1e-1, default_value=1e-4, log=True)
+    cs.add_hyperparameters([shrinkage, shrinkage_factor, n_components, tol])
+
+    cs.add_condition(EqualsCondition(shrinkage_factor, shrinkage, "manual"))
+    return cs
+
+
+def eval_func(params, x, y):
+    params = params.get_dictionary()
+    model = LDA(**params)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=1)
+    model.fit(x_train, y_train)
+    y_pred = model.predict(x_test)
+    return 1 - balanced_accuracy_score(y_test, y_pred)
+
+
+class LDA:
+    def __init__(self, shrinkage, n_components, tol, shrinkage_factor=0.5, random_state=None):
+        self.shrinkage = shrinkage
+        self.n_components = n_components
+        self.tol = tol
+        self.shrinkage_factor = shrinkage_factor
+        self.estimator = None
+        self.time_limit = None
+        self.random_state = random_state
+
+    def fit(self, X, Y):
+        import sklearn.multiclass
+        from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+        # In case of nested shrinkage
+        if isinstance(self.shrinkage, tuple):
+            self.shrinkage_factor = self.shrinkage[1]['shrinkage_factor']
+            self.shrinkage = self.shrinkage[0]
+
+        if check_none(self.shrinkage):
+            self.shrinkage_ = None
+            solver = 'svd'
+        elif self.shrinkage == "auto":
+            self.shrinkage_ = 'auto'
+            solver = 'lsqr'
+        elif self.shrinkage == "manual":
+            self.shrinkage_ = float(self.shrinkage_factor)
+            solver = 'lsqr'
+        else:
+            raise ValueError(self.shrinkage)
+
+        self.n_components = int(self.n_components)
+        self.tol = float(self.tol)
+
+        estimator = LinearDiscriminantAnalysis(
+            n_components=self.n_components, shrinkage=self.shrinkage_,
+            tol=self.tol, solver=solver)
+
+        if len(Y.shape) == 2 and Y.shape[1] > 1:
+            self.estimator = sklearn.multiclass.OneVsRestClassifier(estimator, n_jobs=1)
+        else:
+            self.estimator = estimator
+
+        self.estimator.fit(X, Y)
+        return self
+
+    def predict(self, X):
+        if self.estimator is None:
+            raise NotImplementedError()
+        return self.estimator.predict(X)
+
+
+dataset_list = dataset_str.split(',')
+check_datasets(dataset_list)
+cs = get_cs()
+
+for dataset in dataset_list:
+    node = load_data(dataset, '../soln-ml/', True, task_type=0)
+    _x, _y = node.data[0], node.data[1]
+    eval = partial(eval_func, x=_x, y=_y)
+    bo = BO(eval, cs, max_runs=run_count, time_limit_per_trial=600, rng=np.random.RandomState(1))
+    bo.run()
+    with open('logs/%s-lda-%d.pkl' % (dataset, run_count), 'wb')as f:
+        pickle.dump(bo.get_history().data, f)
