c8495f75a45850627a042c78d0edc2b4131b5f82
thomas-young-2013 <1225646303@qq.com>
2020-10-19 01:14:56 +0800

    update benchmark.

--- tools/studies/gp_fusion.py
+++ tools/studies/gp_fusion.py
@@ -194,7 +194,7 @@ if __name__ == "__main__":
                 print('Source problems used', source_ids)
 
         if run_num == len(hpo_ids):
-            mth_file = '%s_%s_%d_%d_%s_%s.pkl' % (mth, algo_id, n_src_data, trial_num, surrogate_type, task_id)
+            mth_file = '%s_%s_%d_%d_%s_%s_%d.pkl' % (mth, algo_id, n_src_data, trial_num, surrogate_type, task_id, seed)
             with open(exp_dir + mth_file, 'wb') as f:
                 data = [np.array(exp_results), np.mean(exp_results, axis=0)]
                 pickle.dump(data, f)
