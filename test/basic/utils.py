189262a1718e120a2c06acad0cb59bc8208245a3
Jhj <jianghuaijun0411@126.com>
2022-01-17 16:24:18 +0800

    add load data

--- test/basic/utils.py
+++ test/basic/utils.py
@@ -1,3 +1,7 @@
+import os
+import numpy as np
+import pandas as pd
+
 
 def color_str(s, color):
     return escape_codes[color] + s + escape_codes['reset']
@@ -38,3 +42,55 @@ for name, code in escape_codes_foreground.items():
     escape_codes["%s" % name] = esc(code)
     escape_codes["bold_%s" % name] = esc(1, code)
     escape_codes["thin_%s" % name] = esc(2, code)
+
+
+def load_data(dataset, data_dir):
+    """
+    todo: not finished: label encoding...
+    """
+    data_path = os.path.join(data_dir, "%s.csv" % dataset)
+
+    # Load train data.
+    if dataset in ['higgs', 'amazon_employee', 'spectf', 'usps', 'vehicle_sensIT', 'codrna']:
+        label_col = 0
+    elif dataset in ['rmftsa_sleepdata(1)']:
+        label_col = 1
+    else:
+        label_col = -1
+
+    if dataset in ['spambase', 'messidor_features']:
+        header = None
+    else:
+        header = 'infer'
+
+    if dataset in ['winequality_white', 'winequality_red']:
+        sep = ';'
+    else:
+        sep = ','
+
+    na_values = ["n/a", "na", "--", "-", "?"]
+    keep_default_na = True
+    df = pd.read_csv(data_path, keep_default_na=keep_default_na,
+                     na_values=na_values, header=header, sep=sep)
+
+    # Drop the row with all NaNs.
+    df.dropna(how='all')
+
+    # Clean the data where the label columns have nans.
+    columns_missed = df.columns[df.isnull().any()].tolist()
+
+    label_colname = df.columns[label_col]
+
+    if label_colname in columns_missed:
+        labels = df[label_colname].values
+        row_idx = [idx for idx, val in enumerate(labels) if np.isnan(val)]
+        # Delete the row with NaN label.
+        df.drop(df.index[row_idx], inplace=True)
+
+    train_y = df[label_colname].values
+
+    # Delete the label column.
+    df.drop(label_colname, axis=1, inplace=True)
+
+    train_X = df
+    return train_X, train_y
